b5fa7beb65838dbea07d106693616c95b4f53b40
DragonGawain <craigk00@hotmail.com>
2023-11-04 16:11:20 -0400

    Fixed camera

--- Assets/Scripts/AutoControllers/Camera.cs
+++ Assets/Scripts/AutoControllers/Camera.cs
@@ -14,7 +14,10 @@ public class Camera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.Lerp(Cat.transform.position, Rabbit.transform.position, 1 * Time.fixedDeltaTime);
-        transform.position = new Vector3(transform.position.x, transform.position.y, -10);
+        transform.position = Vector3.Lerp(
+            transform.position,
+            Vector3.Lerp(Cat.transform.position, Rabbit.transform.position, 0.5f),
+            Time.fixedDeltaTime
+        );
     }
 }
