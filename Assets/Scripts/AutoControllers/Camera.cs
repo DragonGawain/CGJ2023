2f3f32f917a9eae10dd89887ec6557a38ee51453
DragonGawain <craigk00@hotmail.com>
2023-11-04 16:13:16 -0400

    Fixed camera again

--- Assets/Scripts/AutoControllers/Camera.cs
+++ Assets/Scripts/AutoControllers/Camera.cs
@@ -19,5 +19,6 @@ public class Camera : MonoBehaviour
             Vector3.Lerp(Cat.transform.position, Rabbit.transform.position, 0.5f),
             Time.fixedDeltaTime
         );
+        transform.position = new Vector3(transform.position.x, transform.position.y, -10);
     }
 }
