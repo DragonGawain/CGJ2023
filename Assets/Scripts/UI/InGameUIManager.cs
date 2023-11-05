30c9bd2d750b1e1ebe803c9fe23556c1d9d5117a
Tristan Lafleur <70395022+Trim0500@users.noreply.github.com>
2023-11-05 12:28:34 -0500

    CGJ2023[Art]: In game UI refinement (awaiting other icons)

--- Assets/Scripts/UI/InGameUIManager.cs
+++ Assets/Scripts/UI/InGameUIManager.cs
@@ -15,6 +15,8 @@ public class InGameUIManager : MonoBehaviour
     public GameObject clearScreenPage;
     public GameObject clearDefaultSelected;
     public GameObject timeDisplayText;
+    public Image tierDisplayImage;
+    public List<Sprite> tierSprites;
     public GameObject SubmitNavigationEffect;
     public GameObject BaseNavigationEffect;
     public GameObject levelClearEffect;
@@ -68,7 +70,18 @@ public class InGameUIManager : MonoBehaviour
         var seconds = Mathf.Ceil(referenceTime - currentTimeElapsed % 60);
         var timeDisplay = $"{minutes}:{seconds}";
 
-        // Need to show rank image
+        if (tier == 1)
+        {
+            tierDisplayImage.sprite = tierSprites[0];
+        }
+        else if (tier == 2)
+        {
+            tierDisplayImage.sprite = tierSprites[1];
+        }
+        else if (tier == 3)
+        {
+            tierDisplayImage.sprite = tierSprites[2];
+        }
 
         clearScreenPage.SetActive(true);
 
