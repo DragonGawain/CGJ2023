1208e7d8fca3a08e6a291b1de6587a1a3eb3517f
5DollarCostcoChicken <kiran.freund8@gmail.com>
2023-11-04 23:30:46 -0400

    Created Prefabs for Level Blocks

--- Assets/Scripts/AutoControllers/MovingBlock.cs
+++ Assets/Scripts/AutoControllers/MovingBlock.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class MovingBlock : MonoBehaviour
 {
+    [SerializeField, Range(-50f, 50f)]
+    float distanceX;
+    [SerializeField, Range(-50f, 50f)]
+    float distanceY;
+    Vector3 initialPosition;
     // Start is called before the first frame update
     void Start()
     {
-        
+        initialPosition = transform.position;
+        StartCoroutine(move());
     }
-
-    // Update is called once per frame
-    void Update()
+    IEnumerator move()
     {
-        
+        for (int i = 0; i < (int)(Mathf.Max(Mathf.Abs(distanceY), Mathf.Abs(distanceX)) * 10); i++)
+        {
+            transform.position = new Vector3(transform.position.x + distanceX / 200f, transform.position.y + distanceY / 200f, transform.position.z);
+            yield return new WaitForSecondsRealtime(.01f);
+        }
+        transform.position = initialPosition;
+        StartCoroutine(move());
     }
 }
